b86daca841fe1fbf9a90186e4c8427f7b03f2a9a
ERIC10000 <erickolattiz@gmail.com>
2023-10-05 11:49:00 -0700

    Initial commit

---
+++ app/src/main/java/com/example/managerapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.managerapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tvNotifyPickers  = findViewById<TextView>(R.id.NotifyPickers)
+        tvNotifyPickers.setOnClickListener {
+            val intent = Intent(applicationContext, PickerNotification::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/managerapp/PickerNotification.kt
@@ -0,0 +1,11 @@
+package com.example.managerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PickerNotification : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_picker_notification)
+    }
+}
